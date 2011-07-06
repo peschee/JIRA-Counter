316c944da392601cc633ef1f4aee657ae7a1285f
Peter Siska <pesche@gridonic.ch>
2011-07-06 15:27:21 +0200

    Upped version number, changed name since the functions are generalized now

--- jira-counter_chrome/jira.js
+++ jira-counter_chrome/jira.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name           JIRA Story Points
 // @namespace      http://pesche.name
-// @description    Counts the number of story points in a JIRA filter.
-// @include        https://jira.*
+// @description    Counts the number of story points (and some other things) in JIRA filters.
+// @include        https://jira*
 // @require        https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js
-// @version        1.0.3
+// @version        1.0.4
 // @icon           http://f.cl.ly/items/122R0b1n3Q1w1j0A2o3S/text_list_numbers.png
 // ==/UserScript==
 
